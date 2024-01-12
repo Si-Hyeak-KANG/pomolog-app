0ce9c771032c96d498982df3c65b09ed33a366cd
Si-Hyeak-KANG <zlcls456@gmail.com>
2024-01-12 21:56:22 +0900

    글 작성 클래스 작성

---
+++ lib/screens/write_todo_screen.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class WriteTodoScreen extends StatelessWidget {
+  const WriteTodoScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      backgroundColor: Color(0xffDF4F47),
+      body: Padding(
+        padding: EdgeInsets.all(40),
+        child: Text("text"),
+      ),
+    );
+  }
+}
