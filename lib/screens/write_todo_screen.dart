f8d7113dc9d8765925d638bc53a5ef04cabadfb2
Si-Hyeak-KANG <zlcls456@gmail.com>
2024-01-18 16:37:12 +0900

    todo 작성 UI 수정

--- lib/screens/write_todo_screen.dart
+++ lib/screens/write_todo_screen.dart
@@ -6,20 +6,23 @@ class WriteTodoScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      resizeToAvoidBottomInset: false,
       backgroundColor: const Color(0xffDF4F47),
       appBar: AppBar(
         elevation: 0,
         backgroundColor: const Color(0xffDF4F47),
         foregroundColor: Colors.white,
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(40),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            //const SizedBox(height: 50),
-            // 할 일 INPUT
-            TextFormField(
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          //const SizedBox(height: 50),
+          // 할 일 INPUT
+          Padding(
+            padding: const EdgeInsets.all(30.0),
+            child: TextFormField(
+              onTapOutside: (event) =>
+                  FocusManager.instance.primaryFocus?.unfocus(),
               decoration: const InputDecoration(
                 enabledBorder: UnderlineInputBorder(
                   borderSide: BorderSide(
@@ -36,11 +39,14 @@ class WriteTodoScreen extends StatelessWidget {
                 labelText: '할 일을 입력해주세요.',
               ),
             ),
+          ),
 
-            const SizedBox(height: 60),
+          const SizedBox(height: 30),
 
-            //  목표 뽀모도로 설정
-            const Text(
+          //  목표 뽀모도로 설정
+          const Padding(
+            padding: EdgeInsets.symmetric(horizontal: 30),
+            child: Text(
               "목표 뽀모도로",
               style: TextStyle(
                 color: Colors.white,
@@ -48,8 +54,11 @@ class WriteTodoScreen extends StatelessWidget {
                 fontSize: 20,
               ),
             ),
-            const SizedBox(height: 10),
-            SizedBox(
+          ),
+          const SizedBox(height: 10),
+          Padding(
+            padding: const EdgeInsets.all(5.0),
+            child: SizedBox(
               height: 60,
               child: Expanded(
                 child: ListView.separated(
@@ -65,39 +74,52 @@ class WriteTodoScreen extends StatelessWidget {
                         borderRadius: BorderRadius.circular(120),
                       ),
                       child: Center(
-                          child: Text(
-                        '${index + 1}',
-                        style: const TextStyle(
-                          fontSize: 20,
-                          fontWeight: FontWeight.w900,
-                          color: Color(0xffDF4F47),
+                        child: Text(
+                          '${index + 1}',
+                          style: const TextStyle(
+                            fontSize: 20,
+                            fontWeight: FontWeight.w900,
+                            color: Color(0xffDF4F47),
+                          ),
                         ),
-                      )),
+                      ),
                     );
                   },
                 ),
               ),
             ),
-            const SizedBox(height: 50),
-            Center(
-              child: ElevatedButton.icon(
-                onPressed: () {},
-                icon: const Icon(
-                  Icons.add,
-                  size: 24,
-                  color: Color(0xffDF4F47),
-                ),
-                label: const Text(
-                  "등록",
-                  style: TextStyle(
-                      color: Color(0xffDF4F47),
-                      fontSize: 18,
-                      fontWeight: FontWeight.w600),
+          ),
+          const Spacer(),
+          Center(
+            child: ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                minimumSize: const Size(100, 80),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(0),
                 ),
               ),
-            )
-          ],
-        ),
+              onPressed: () => Navigator.pop(context),
+              child: const Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Icon(
+                    Icons.add,
+                    size: 24,
+                    color: Color(0xffDF4F47),
+                  ),
+                  Text(
+                    "등록",
+                    style: TextStyle(
+                        color: Color(0xffDF4F47),
+                        fontSize: 18,
+                        fontWeight: FontWeight.w600),
+                  ),
+                ],
+              ),
+            ),
+          ),
+          SizedBox(height: MediaQuery.of(context).viewInsets.bottom),
+        ],
       ),
     );
   }
