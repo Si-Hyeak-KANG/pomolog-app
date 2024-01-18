fbb6ba86d0e2d181c629a88cf336add6de8b1575
Si-Hyeak-KANG <zlcls456@gmail.com>
2024-01-18 20:16:05 +0900

    Todo 홈 - 모달창 게이지바 구현

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:percent_indicator/linear_percent_indicator.dart';
 import 'package:pomolog/screens/write_todo_screen.dart';
 
 class HomeScreen extends StatelessWidget {
@@ -12,7 +13,7 @@ class HomeScreen extends StatelessWidget {
         padding: const EdgeInsets.all(40),
         child: Column(
           children: [
-            const SizedBox(height: 60),
+            //const SizedBox(height: 0),
 
             // 'TODO' 타이틀
             const Center(
@@ -27,7 +28,7 @@ class HomeScreen extends StatelessWidget {
 
             const Center(
               child: Text(
-                "오늘의 뽀모도로 : 5",
+                "오늘 흭득한 뽀모도로 : 5",
                 style: TextStyle(
                   color: Colors.white,
                   fontSize: 16,
@@ -97,18 +98,121 @@ class HomeScreen extends StatelessWidget {
                             context: context,
                             builder: (BuildContext context) {
                               return Container(
+                                height: 400,
                                 color: Colors.white,
                                 child: Center(
-                                  child: Column(
-                                    mainAxisAlignment: MainAxisAlignment.center,
-                                    mainAxisSize: MainAxisSize.min,
-                                    children: <Widget>[
-                                      const Text("오늘의 할 일!"),
-                                      ElevatedButton(
-                                        child: const Text('Focus ON!'),
-                                        onPressed: () => Navigator.pop(context),
-                                      )
-                                    ],
+                                  child: Padding(
+                                    padding: const EdgeInsets.all(15.0),
+                                    child: Column(
+                                      crossAxisAlignment:
+                                          CrossAxisAlignment.center,
+                                      children: [
+                                        Column(
+                                          crossAxisAlignment:
+                                              CrossAxisAlignment.start,
+                                          children: [
+                                            const Padding(
+                                              padding: EdgeInsets.all(5.0),
+                                              child: Row(
+                                                mainAxisAlignment:
+                                                    MainAxisAlignment
+                                                        .spaceBetween,
+                                                children: [
+                                                  Icon(
+                                                    Icons
+                                                        .check_box_outline_blank_rounded,
+                                                    color: Color(0xffDF4F47),
+                                                    size: 30,
+                                                  ),
+                                                  Icon(
+                                                    Icons.edit,
+                                                    color: Color(0xffDF4F47),
+                                                    size: 30,
+                                                  ),
+                                                ],
+                                              ),
+                                            ),
+                                            const SizedBox(height: 10),
+                                            Container(
+                                              decoration: const BoxDecoration(
+                                                border: Border(
+                                                  bottom: BorderSide(
+                                                    color: Colors.grey,
+                                                  ),
+                                                ),
+                                              ),
+                                            ),
+                                            const SizedBox(height: 20),
+                                            const Padding(
+                                              padding: EdgeInsets.symmetric(
+                                                  horizontal: 18.0),
+                                              child: Text(
+                                                "오늘의 할 일!!!",
+                                                style: TextStyle(
+                                                  fontSize: 20,
+                                                ),
+                                              ),
+                                            ),
+                                          ],
+                                        ),
+                                        const SizedBox(height: 40),
+                                        Column(
+                                          children: [
+                                            Container(
+                                              padding:
+                                                  const EdgeInsets.symmetric(
+                                                      vertical: 2.0),
+                                              alignment:
+                                                  const FractionalOffset(0, 1),
+                                              child: FractionallySizedBox(
+                                                  child: Image.asset(
+                                                '/Users/james/dev/app/pomolog/assets/icons/tomato.png',
+                                                width: 30,
+                                                height: 30,
+                                                fit: BoxFit.contain,
+                                              )),
+                                            ),
+                                            LinearPercentIndicator(
+                                              padding: EdgeInsets.zero,
+                                              percent: 0,
+                                              lineHeight: 30,
+                                              backgroundColor: Colors.black38,
+                                              progressColor:
+                                                  const Color(0xffDF4F47),
+                                              width: 360,
+                                            ),
+                                            const Row(
+                                              mainAxisAlignment:
+                                                  MainAxisAlignment.end,
+                                              children: [
+                                                Text(
+                                                  "(0/5)",
+                                                  style: TextStyle(
+                                                    fontSize: 16,
+                                                  ),
+                                                )
+                                              ],
+                                            )
+                                          ],
+                                        ),
+                                        const SizedBox(height: 50),
+                                        ElevatedButton(
+                                          style: ElevatedButton.styleFrom(
+                                            minimumSize: const Size(50, 50),
+                                            shape: RoundedRectangleBorder(
+                                              borderRadius:
+                                                  BorderRadius.circular(0),
+                                            ),
+                                            backgroundColor:
+                                                const Color(0xffDF4F47),
+                                            foregroundColor: Colors.white,
+                                          ),
+                                          child: const Text('집중 시작'),
+                                          onPressed: () =>
+                                              Navigator.pop(context),
+                                        )
+                                      ],
+                                    ),
                                   ),
                                 ),
                               );
